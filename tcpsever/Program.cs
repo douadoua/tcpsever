ac2e13390b95f27b70f41ab50f5d90d59994bd5a
unknown <2661181709@  qq.com>
2017-11-10 09:28:57 +0800

    Initial commit

---
+++ tcpsever/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace tcpsever
+{
+    class Program
+    {
+        static List<Client> clientlist = new List<Client>();
+        static void Main(string[] args)
+        {
+            Socket tcpsever = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            tcpsever.Bind(new IPEndPoint(IPAddress.Parse("192.168.0.107"),8899));
+
+            tcpsever.Listen(100);
+            Console.WriteLine("sever runnig...");
+
+            while (true)
+            {
+                Socket clientsocket = tcpsever.Accept();
+                Console.WriteLine("a client is connected !");
+                Client client = new Client(clientsocket);//
+                clientlist.Add(client);
+            }
+            
+        }
+    }
+}
---
+++ tcpsever/client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+
+namespace tcpsever
+{
+    class Client
+    {
+        private Socket clientsocket;
+
+        public Client(Socket s)
+        {
+            clientsocket = s;
+        }
+    }
+}
